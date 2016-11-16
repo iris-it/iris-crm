d0b89b9102e97facac9500d6dd38b21392ce1c7d
Alexandre Mangin <alexandre.mangin@viacesi.fr>
2016-11-16 11:45:04 +0100

    removed height for home page charts ( not suitable for mobile or small screens )

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -97,19 +97,7 @@
                         <h2 class="box-title text-uppercase">{{trans('app.dashboard:sales-chart-title')}}</h2>
 
                         <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                            </button>
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-box-tool dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-wrench"></i></button>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">Separated link</a></li>
-                                </ul>
-                            </div>
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                             <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
                         </div>
                     </div>
@@ -119,7 +107,7 @@
                             <div class="col-md-8">
                                 <div class="chart">
                                     <!-- Sales Chart Canvas -->
-                                    <canvas id="lineChart" width="600" height="500"></canvas>
+                                    <canvas id="lineChart" width="600"></canvas>
                                 </div>
                                 <!-- /.chart-responsive -->
                             </div>
