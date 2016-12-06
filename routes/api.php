5f4964c3a7f6fa9cc1773385f0288ab7ba68b2c5
monkeyC <alan.corbel@viacesi.fr>
2016-12-06 15:27:59 +0100

    Inb4 component modification
    Invoices / Quotes system for offices tab
    
    TODO :
    Serialization for Invoices & Quotes
    
    Think about a way to load invoices & quotes of an office more AJAXly

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+
+Route::group(['namespace' => 'Api'],function(){
+
+    Route::get('office/{id}/quotes', 'OfficeController@fetchQuotes');
+
+});
