0f3c92a8efa7837d9423911dc732487b1e1ce517
Alexandre Mangin <alexandre.mangin@viacesi.fr>
2016-12-20 15:27:39 +0100

    add garde fou pour seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,10 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleTableSeeder::class);
         $this->call(LicenceTableSeeder::class);
 
-        $this->call(OrganizationTableSeeder::class);
-        $this->call(AccountTableSeeder::class);
-        $this->call(OfficeTableSeeder::class);
+        if (env('APP_ENV' !== 'production')) {
+            $this->call(OrganizationTableSeeder::class);
+            $this->call(AccountTableSeeder::class);
+            $this->call(OfficeTableSeeder::class);
+        }
     }
 }
